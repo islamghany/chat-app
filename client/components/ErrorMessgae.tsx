bc2bd41770e101ff8aab58ed8196f9318544fdfd
Islam Mostafa <islamghany3@gmail.com>
2020-09-02 14:48:54 +0200

    final basics

--- client/components/ErrorMessgae.tsx
+++ client/components/ErrorMessgae.tsx
@@ -1,13 +1,13 @@
 import { Message } from "rsuite";
 
-const ErrorMessage = ({ error }) => {
+const ErrorMessage = ({ error }:any) => {
   if (!error || !error.message) return null;
   if (
     error.networkError &&
     error.networkError.result &&
     error.networkError.result.errors.length
   ) {
-    return error.networkError.result.errors.map((error, i) => (
+    return error.networkError.result.errors.map((error:any, i:number) => (
       <Message
         key={i}
         type="error"
